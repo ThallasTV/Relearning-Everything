2d8d657b2e5938cf5e032795b1db17a881c7a910
Joshua Eatwell <josh-eatwell@hotmail.com>
2023-01-01 18:39:12 +0000

    Test
    Throwing the project on here

---
+++ RelearningTime/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Classes;
+
+public class BankAccount
+{
+    public string Name { get;}
+    public string Owner { get;}
+    public decimal Balance { get;}
+}
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Bonjour");
+    }
+}
